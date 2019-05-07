695da198877a8bdec276f046845fc50a25777b42
Joshua Letzter <joshualetzter@IvyBoy.attlocal.net>
2019-05-07 18:20:57 -0400

    random powershell scripts

---
+++ check_if_active.ps1
@@ -0,0 +1,13 @@
+﻿$users = get-content C:\path_to\usernames.csv
+foreach ($item in $users) {
+$user = $null
+$user =  Get-aduser -filter {samAccountName -eq $item}
+if ($user)
+    {
+    get-ADUser $user | select samaccountname,name,enabled | Export-Csv C:\path_to\output.csv -nti -Append
+    }
+    else
+    {
+    "$item ,,N/A" | Add-Content C:\path_to\output.csv
+    }
+}
